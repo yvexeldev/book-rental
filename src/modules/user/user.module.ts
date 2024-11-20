513b4623e9bc76e52f60328d6b91aedd06d42c6a
yvexeldev <azamabdusalomov@icloud.com>
2024-11-20 14:47:14 +0500

    usermodule created

---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+  ],
+})
+export class UserModule {}
