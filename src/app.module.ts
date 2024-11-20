81171f47bc993c5c0348437380f969d5629afef5
yvexeldev <azamabdusalomov@icloud.com>
2024-11-20 15:37:44 +0500

    i18n added to appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,37 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { IndexModule } from './modules';
 import { ConfigSchema } from './utils/config';
+import * as path from 'path';
 
+import {
+  I18nModule,
+  AcceptLanguageResolver,
+  QueryResolver,
+  HeaderResolver,
+} from 'nestjs-i18n';
 @Module({
   imports: [
     IndexModule,
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: ConfigSchema,
+    }),
+
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        fallbackLanguage: configService.get<string>('I18N_FALLBACK_LANGUAGE'),
+        loaderOptions: {
+          path: path.join(__dirname, '.././i18n/'),
+          watch: true,
+        },
+      }),
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+        new HeaderResolver(['x-lang']),
+      ],
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
