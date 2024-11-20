a7ed47dd9bac58fbcbebae904768b875fccebce3
yvexeldev <azamabdusalomov@icloud.com>
2024-11-20 15:38:56 +0500

    I18N Fallback lang added to config schema

--- src/utils/config/config.schema.ts
+++ src/utils/config/config.schema.ts
@@ -4,6 +4,7 @@ export const ConfigSchema = Joi.object({
   PORT: Joi.number().min(0).max(65535).default(3000),
   DATABASE_URL: Joi.string().uri().required(),
   JWT_SECRET: Joi.string().required().default('jwtS3cr3t$$'),
-  HOST: Joi.string().uri().default('http://localhost'),
+  HOST: Joi.string().default('localhost'),
   JWT_EXPIRES_IN: Joi.string().default('7d'),
+  I18N_FALLBACK_LANGUAGE: Joi.string().default('en'),
 });
