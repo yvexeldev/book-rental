be8914b7c8578539f4c07711692fe4b8983bd826
yvexeldev <azamabdusalomov@icloud.com>
2024-11-21 10:39:08 +0500

    Main UtilsModule for all utils

---
+++ src/utils/utils.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma';
+import { RedisModule } from './redis/redis.module';
+import { MailModule } from './mail/mail.module';
+
+@Module({
+  imports: [PrismaModule, RedisModule, MailModule],
+})
+export class UtilsModule {}
