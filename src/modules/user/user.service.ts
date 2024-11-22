ae859800fc0fb875233351b6a62fe94502492c79
yvexeldev <azamabdusalomov@icloud.com>
2024-11-22 10:57:45 +0500

    minor bugs, error handlers fixed, updateUser, setNickname functionality added

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,19 @@
 import {
   BadRequestException,
+  ForbiddenException,
   Injectable,
   Logger,
   NotFoundException,
 } from '@nestjs/common';
 import { BaseResponse, IUserService } from './user.interface';
 import { I18nService } from 'nestjs-i18n';
-import { SignInDto, SignUpDto, VerifyOtpDto } from './dto/user.dto';
+import {
+  SetUsernameDto,
+  SignInDto,
+  SignUpDto,
+  UpdateUserDto,
+  VerifyOtpDto,
+} from './dto/user.dto';
 import { JwtService } from '@nestjs/jwt';
 import { RedisService } from '../../utils/redis/redis.service';
 import * as crypto from 'node:crypto';
@@ -15,6 +22,9 @@ import { $Enums, User } from '@prisma/client';
 import * as bcrypt from 'bcryptjs';
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { RABBITMQ } from '../../utils/config/constants';
+import { when } from 'joi';
+
+class SetUserNameDto {}
 
 @Injectable()
 export class UserService implements IUserService {
@@ -151,25 +161,104 @@ export class UserService implements IUserService {
           data: { token },
         };
       } else {
-        return {
-          message: this.i18n.t('user.OTP_INCORRECT'),
-          data: { verifyOtpDto },
-        };
+        throw new ForbiddenException(this.i18n.t('user.OTP_INCORRECT'));
       }
     });
   }
 
-  async getUserById(id: number): Promise<BaseResponse | User> {
+  async getUserById(id: number): Promise<User> {
     const user = await this.prismaService.user.findFirst({
       where: { id, isVerified: true },
     });
 
     if (!user) {
-      return {
-        message: this.i18n.t('user.NOT_FOUND'),
-        data: {},
-      };
+      throw new NotFoundException(this.i18n.t('user.NOT_FOUND'));
     }
     return user;
+  }
+
+  async updateUser(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<BaseResponse> {
+    return this.prismaService.$transaction(async (prisma) => {
+      if (updateUserDto.password) {
+        updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+      }
+      const isExists = await prisma.user.findFirst({
+        where: { id, isVerified: true },
+      });
+      Logger.log({ isExists });
+      if (isExists) {
+        const user = await prisma.user.update({
+          where: { id },
+          data: updateUserDto,
+        });
+
+        Logger.log({ user, updateUserDto });
+
+        return {
+          message: this.i18n.t('user.UPDATE'),
+          data: { updatedUser: user },
+        };
+      } else {
+        throw new NotFoundException(this.i18n.t('user.NOT_FOUND'));
+      }
+    });
+  }
+
+  async setUsername(
+    id: number,
+    setUsernameDto: SetUsernameDto,
+  ): Promise<BaseResponse> {
+    const user = await this.prismaService.user.findFirst({
+      where: { id, isVerified: true },
+    });
+    if (!user) {
+      throw new NotFoundException(this.i18n.t('user.NOT_FOUND'));
+    }
+    const isExists = await this.prismaService.user.findFirst({
+      where: { username: setUsernameDto.username },
+    });
+    if (isExists) {
+      throw new BadRequestException(this.i18n.t('user.ALREADY_EXISTS'));
+    }
+    await this.prismaService.user.update({
+      where: {
+        id,
+      },
+      data: { username: setUsernameDto.username },
+    });
+
+    Logger.log({ setUsernameDto });
+
+    return {
+      message: this.i18n.t('user.UPDATE'),
+      data: { updatedUser: user },
+    };
+  }
+
+  async getAllusers(isVerified: boolean): Promise<BaseResponse> {
+    const users = await this.prismaService.user.findMany({
+      where: { isVerified: isVerified },
+    });
+
+    return {
+      message: 'success',
+      data: { users },
+    };
+  }
+
+  async deleteUnverifiedUsers(): Promise<BaseResponse> {
+    const users = await this.prismaService.user.deleteMany({
+      where: {
+        isVerified: false,
+      },
+    });
+
+    return {
+      message: 'success',
+      data: { users },
+    };
   }
 }
