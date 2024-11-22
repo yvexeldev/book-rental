443bf0ca61b922f3dc07afcacfedb0e8423de35c
yvexeldev <azamabdusalomov@icloud.com>
2024-11-22 09:09:48 +0500

    user interface updates

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,12 +1,41 @@
 import { SignUpDto, SignInDto, VerifyOtpDto } from './dto/user.dto';
+import { User } from '@prisma/client';
 
 export type BaseResponse = {
   message: string;
-  data: object;
+  data: object | null;
 };
 
 export interface IUserService {
+  /**
+   * Handles user sign-up.
+   *
+   * @param signUpDto - The DTO containing user sign-up data.
+   * @returns A promise that resolves to a base response with user information.
+   */
   signUp(signUpDto: SignUpDto): Promise<BaseResponse>;
+
+  /**
+   * Handles user sign-in.
+   *
+   * @param signInDto - The DTO containing user sign-in data.
+   * @returns A promise that resolves to a base response with a JWT token.
+   */
   signIn(signInDto: SignInDto): Promise<BaseResponse>;
-  verifyOtp(verifyOtpDto: VerifyOtpDto): Promise<BaseResponse>;
+
+  /**
+   * Retrieves a user by their email address.
+   *
+   * @param email - The user's email address.
+   * @returns A promise that resolves to a user object or null if not found.
+   */
+  getUserByEmail(email: string): Promise<User | null>;
+
+  /**
+   * Retrieves a user by their email address.
+   *
+   * @param id - The user's email address.
+   * @returns A promise that resolves to a user object or null if not found.
+   */
+  getUserById(id: number): Promise<User | BaseResponse>;
 }
