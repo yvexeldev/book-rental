023f6ecda3be62c5ffc730aca9f499092bbf1d7d
yvexeldev <azamabdusalomov@icloud.com>
2024-11-26 09:29:25 +0500

    book service interface

---
+++ src/modules/book/book.interface.ts
@@ -0,0 +1,12 @@
+import { BaseResponse } from '../../utils/config/interfaces';
+import { Book } from '@prisma/client';
+import { CreateBookDto, UpdateBookDto } from './dto/book.dto';
+
+export interface IBookService {
+  getAll(page?: number, limit?: number, search?: string): Promise<BaseResponse>;
+  getByISBN(isbn: string): Promise<Book | null>;
+  getOne(id: number): Promise<Book>;
+  createBook(createBookDto: CreateBookDto): Promise<Book>;
+  updateBook(id: number, updateBookDto: UpdateBookDto): Promise<Book>;
+  deleteBook(id: number): Promise<Book>;
+}
