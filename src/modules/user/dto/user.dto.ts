1784f0fc40891048198cdb2ad7a4d4850c3d1b6f
yvexeldev <azamabdusalomov@icloud.com>
2024-11-26 10:56:20 +0500

    user dto validation messages

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -8,77 +8,172 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
 
 export class SignUpDto {
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2)
-  @MaxLength(75)
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_FIRST_NAME_STRING'),
+  })
+  @IsNotEmpty({
+    message: i18nValidationMessage('validation.VALIDATION_FIRST_NAME_STRING'),
+  })
+  @MinLength(2, {
+    message: i18nValidationMessage(
+      'validation.VALIDATION_FIRST_NAME_MIN_LENGTH',
+    ),
+  })
+  @MaxLength(75, {
+    message: i18nValidationMessage(
+      'validation.VALIDATION_FIRST_NAME_MAX_LENGTH',
+    ),
+  })
   firstName: string;
 
   @IsOptional()
-  @IsString()
-  @MinLength(2)
-  @MaxLength(75)
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_LAST_NAME_STRING'),
+  })
+  @MinLength(2, {
+    message: i18nValidationMessage(
+      'validation.VALIDATION_LAST_NAME_MIN_LENGTH',
+    ),
+  })
+  @MaxLength(75, {
+    message: i18nValidationMessage(
+      'validation.VALIDATION_LAST_NAME_MAX_LENGTH',
+    ),
+  })
   lastName?: string;
 
-  @IsString()
-  @IsEmail()
-  @MaxLength(255)
-  @IsNotEmpty()
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID'),
+  })
+  @IsEmail(
+    {},
+    { message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID') },
+  )
+  @MaxLength(255, {
+    message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID'),
+  })
+  @IsNotEmpty({
+    message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID'),
+  })
   email: string;
 
-  @IsString()
-  @IsStrongPassword()
-  @IsNotEmpty()
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_PASSWORD_STRONG'),
+  })
+  @IsStrongPassword(
+    {},
+    { message: i18nValidationMessage('validation.VALIDATION_PASSWORD_STRONG') },
+  )
+  @IsNotEmpty({
+    message: i18nValidationMessage('validation.VALIDATION_PASSWORD_REQUIRED'),
+  })
   password: string;
 }
 
 export class SignInDto {
-  @IsString()
-  @IsNotEmpty()
-  @IsEmail()
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID'),
+  })
+  @IsNotEmpty({
+    message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID'),
+  })
+  @IsEmail(
+    {},
+    { message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID') },
+  )
   email: string;
 
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({
+    message: i18nValidationMessage('validation.VALIDATION_PASSWORD_REQUIRED'),
+  })
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_PASSWORD_REQUIRED'),
+  })
   password: string;
 }
 
 export class VerifyOtpDto {
-  @IsNumber()
-  @IsNotEmpty()
+  @IsNumber(
+    {},
+    { message: i18nValidationMessage('validation.VALIDATION_OTP_REQUIRED') },
+  )
+  @IsNotEmpty({
+    message: i18nValidationMessage('validation.VALIDATION_OTP_REQUIRED'),
+  })
   otp: number;
 
-  @IsString()
-  @IsNotEmpty()
-  @IsEmail()
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID'),
+  })
+  @IsNotEmpty({
+    message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID'),
+  })
+  @IsEmail(
+    {},
+    { message: i18nValidationMessage('validation.VALIDATION_EMAIL_VALID') },
+  )
   email: string;
 }
 
 export class UpdateUserDto {
-  @IsString()
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_FIRST_NAME_STRING'),
+  })
   @IsOptional()
-  @MinLength(2)
-  @MaxLength(255)
-  firstName: string;
+  @MinLength(2, {
+    message: i18nValidationMessage(
+      'validation.VALIDATION_FIRST_NAME_MIN_LENGTH',
+    ),
+  })
+  @MaxLength(255, {
+    message: i18nValidationMessage(
+      'validation.VALIDATION_FIRST_NAME_MAX_LENGTH',
+    ),
+  })
+  firstName?: string;
 
-  @IsString()
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_LAST_NAME_STRING'),
+  })
   @IsOptional()
-  @MinLength(2)
-  @MaxLength(255)
-  lastName: string;
+  @MinLength(2, {
+    message: i18nValidationMessage(
+      'validation.VALIDATION_LAST_NAME_MIN_LENGTH',
+    ),
+  })
+  @MaxLength(255, {
+    message: i18nValidationMessage(
+      'validation.VALIDATION_LAST_NAME_MAX_LENGTH',
+    ),
+  })
+  lastName?: string;
 
-  @IsString()
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_PASSWORD_STRONG'),
+  })
   @IsOptional()
-  @IsStrongPassword()
-  password: string;
+  @IsStrongPassword(
+    {},
+    { message: i18nValidationMessage('validation.VALIDATION_PASSWORD_STRONG') },
+  )
+  password?: string;
 }
 
 export class SetUsernameDto {
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(5)
-  @MaxLength(20)
+  @IsString({
+    message: i18nValidationMessage('validation.VALIDATION_USERNAME_STRING'),
+  })
+  @IsNotEmpty({
+    message: i18nValidationMessage('validation.VALIDATION_USERNAME_STRING'),
+  })
+  @MinLength(5, {
+    message: i18nValidationMessage('validation.VALIDATION_USERNAME_MIN_LENGTH'),
+  })
+  @MaxLength(20, {
+    message: i18nValidationMessage('validation.VALIDATION_USERNAME_MAX_LENGTH'),
+  })
   username: string;
 }
